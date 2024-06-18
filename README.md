b14c4f41ca44363d06f685f496ea1d536772abd5
IVEWOR <howdy@deepslog.com>
2024-06-18 21:42:02 +0530

    base setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Quote Generator
+
+Generate a random quote from quotable.io api. It uses Axios library for data fetching, useState, useEffect from react to display the quote.
---
+++ src/app/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get("https://api.quotable.io/random");
+      setQuote(response.data.content);
+      setAuthor(response.data.author);
+    } catch (error) {
+      console.error(`Error fetching the data ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <main className="max-w-4xl mx-auto mt-10 bg-slate-900 p-10 rounded-xl">
+      <div className="h-52">
+        <h1 className="text-3xl font-semibold mb-4">
+          Random Quote Generator App
+        </h1>
+        <p className="text-lg">{quote}</p>
+        <p className="italic mt-4">~ {author}</p>
+      </div>
+      <div className="flex">
+        <button
+          onClick={fetchQuote}
+          className="ml-auto bg-indigo-600 p-2 px-5 rounded-lg hover:bg-indigo-700 transition-all"
+        >
+          New Quote
+        </button>
+      </div>
+    </main>
+  );
+}
